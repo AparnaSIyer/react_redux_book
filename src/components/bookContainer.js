96064a151c47948335cafcb5b464c712278232f8
AparnaEm16 <aparnaiyer1695@gmail.com>
2021-01-07 19:12:15 +0530

    initial commit

---
+++ src/components/bookContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function bookContainer() {
+    return (
+        <div>
+            <h2>Number of books</h2>
+            <button>Buy books</button>
+        </div>
+    );
+}
+
+export default bookContainer;
---
+++ src/redux/book/bookActions.js
@@ -0,0 +1,8 @@
+import './bookTypes';
+import { BUY_BOOK } from './bookTypes';
+
+const buyBook = () => {
+    return {
+        type: BUY_BOOK
+    };
+};
